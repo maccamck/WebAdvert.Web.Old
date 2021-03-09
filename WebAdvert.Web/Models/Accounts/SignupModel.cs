21ef4fd8381f49161094100879c5ea1f2764a4b2
macca_mck@hotmail.com <Gladiator65*>
2021-03-09 17:02:23 +0000

    first commit

---
+++ WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+	public class SignupModel
+	{
+		[Required]
+		[EmailAddress]
+		[Display(Name="Email")]
+		public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[StringLength(6,ErrorMessage ="Password must be at least siz characters long!")]
+		[Display(Name ="Password")]
+		public string Password { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare("Password",ErrorMessage ="Passwords do not match")]
+		[Display(Name = "Confirm Password")]
+		public string ConfirmPassword { get; set; }
+	}
+}
